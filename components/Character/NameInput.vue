af324ef40956f57a13f3eb61385e7f5bdfb9ce4b
thalesd <thales.doni.ol@gmail.com>
2024-04-03 13:09:45 -0300

    Makes search inputs behave like forms

--- components/Character/NameInput.vue
+++ components/Character/NameInput.vue
@@ -1,14 +1,20 @@
 <script setup>
     const charName = ref("");
 
-    const handleCreate = () => {
+    const handleCreate = (e) => {
+        e.preventDefault();
         navigateTo(`/create/${charName.value}`);
     }
 </script>
 
 <template>
-    <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-        <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Name your character!" v-model="charName"/>
-        <button class="bg-sky-500 px-10 text-white" @click="handleCreate">Create</button>
-    </div>
+    <form @submit="handleCreate" class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+        <input 
+            name="charName"
+            type="text" 
+            class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" 
+            placeholder="Name your character!" 
+            v-model="charName"/>
+        <button class="bg-sky-500 px-10 text-white" @click="handleCreate">Create</button>        
+    </form>
 </template>
--- components/Character/SearchInput.vue
+++ components/Character/SearchInput.vue
@@ -2,18 +2,20 @@
     const route = useRoute();
     const inputCharName = ref(route.params.characterName);
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
+        e.preventDefault();
         navigateTo(`/characters/${inputCharName.value}`);
     }
 </script>
 
 <template>
-    <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+    <form @submit="handleSearch" class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
         <input 
+            name="charName"
             type="text" 
             class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" 
             placeholder="Search for characters!" 
             v-model="inputCharName"/>
         <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
-    </div>
+    </form>
 </template>
