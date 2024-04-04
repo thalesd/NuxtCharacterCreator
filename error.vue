bbc7d78f88943a3472cd93b2c9c7306f0af1dcf0
thalesd <thales.doni.ol@gmail.com>
2024-04-04 19:47:24 -0300

    Create custom error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const error = useError();
+
+    const handleGoBack = () => {
+        navigateTo('/');
+    }
+</script>
+
+<template>
+    <div class="flex h-screen justify-center items-center flex-col">
+        <h1 class="text-9xl">{{error.statusCode}}</h1>
+        <p class="mt-7 text-4xl">{{error.message}}</p>
+        <button class="bg-black text-white px-4 py-1 mt-2 rounded-lg"
+            @click="handleGoBack">Go Back</button>
+    </div>
+</template>
